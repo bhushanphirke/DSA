5210597c3e868bf240f384c16015b4a271a3a813
Bhushan Phirke <bhushanphirke314@gmail.com>
2026-07-18 15:02:15 +0530

    Arrays

--- Find_missing_optimal.cpp
+++ Find_missing_optimal.cpp
@@ -13,16 +13,13 @@ int optimal_sum(vector<int> arr, int n)
 int optimal_xor(vector<int> arr , int n)
 {
    int xor1=0;
-   for(int i=1;i<=n;i++)
-   {
-     xor1=xor1 ^ i;
-   }
-
    int xor2=0;
    for(int i=0;i<n-1;i++)
    {
       xor2= xor2 ^ arr[i];
+      xor1= xor1 ^ (i+1);
    }
+   xor1=xor1^n;
    return xor1^xor2;
 }
 int main()
